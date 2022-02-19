e6f4a7b05bec5e70a3aa99a7309c5638da5c79b0
Aaron <Aaron@LAPTOP>
2022-02-19 18:39:41 -0500

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiProject;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count+=5;
+		CounterLabel.Text = $"Current count: {count}";
+
+		SemanticScreenReader.Announce(CounterLabel.Text);
+	}
+}
+
